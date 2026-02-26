1b1f223fcad25a8ad178d283fd6534496ff43872
Sumery <xiconz@qq.com>
2026-02-26 17:35:15 +0800

    perf: 创建微服务时,生成的文件名称跟随与微服务名一致

--- internal/data/user.go
+++ internal/data/user.go
@@ -11,34 +11,3 @@ import (
 	"go.uber.org/zap"
 )
 
-var _ biz.UserRepo = (*userRepo)(nil)
-
-type userRepo struct {
-	// queries *models.Queries
-	rdb  *redis.Client
-	auth *casdoorsdk.Client
-	l    *zap.Logger
-}
-
-func NewUserRepo(data *Data, logger *zap.Logger) biz.UserRepo {
-	return &userRepo{
-		// queries: models.New(data.db),
-		rdb:  data.rdb,
-		auth: data.auth,
-		l:    logger,
-	}
-}
-
-func (u userRepo) SignIn(ctx context.Context, req biz.SignInRequest) (*biz.SignInResponse, error) {
-	if u.auth == nil {
-		return nil, errors.New("auth client is nil")
-	}
-	// token, err := u.auth.GetOAuthToken(req.Code, req.State)
-	// if err != nil {
-	// 	return nil, err
-	// }
-	return &biz.SignInResponse{
-		// State: "ok",
-		// Data:  token.AccessToken,
-	}, nil
-}
--- internal/service/user.go
+++ internal/service/user.go
@@ -10,30 +10,3 @@ import (
 	"connectrpc.com/connect"
 )
 
-// UserService 实现 Connect 服务
-type UserService struct {
-	uc *biz.UserUseCase
-}
-
-// 显式接口检查
-var _ userv1connect.UserServiceHandler = (*UserService)(nil)
-
-func NewUserService(uc *biz.UserUseCase) userv1connect.UserServiceHandler {
-	return &UserService{
-		uc: uc,
-	}
-}
-
-func (s *UserService) SignIn(ctx context.Context, c *connect.Request[v1.SignInRequest]) (*connect.Response[v1.SignInResponse], error) {
-	_, err := s.uc.SignIn(
-		ctx,
-		biz.SignInRequest{},
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	response := &v1.SignInResponse{}
-
-	return connect.NewResponse(response), nil
-}
